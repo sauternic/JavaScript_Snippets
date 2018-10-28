73dcf778e8af880d0b5c9b244996c1f2a7051d10
Nicolas Sauter <sauternic@gmail.com>
2018-10-28 16:51:47 +0100

    Update Snippet_1.js

--- Snippet_1.js
+++ Snippet_1.js
@@ -9,10 +9,10 @@ var str = "=53=75=70=70=6F=72=74=49=54=2D=41=75=73=72=C3=BC=73=74=75=6E=67";
 //Hier werden einfach '=' durch '%' ersetzt! ;)
 str = str.replace(/={1}/g, '%');
 
-//Zwischen Ergebniss:
+//Zwischen Ergebnis:
 //"%53%75%70%70%6F%72%74%49%54%2D%41%75%73%72%C3%BC%73%74%75%6E%67"
 
 //Jetzt ist es kompatibel für decodeURI()
 str = decodeURI(str);
 
-//Ergebniss  :  str = "SupportIT-Ausrüstung"
+//Ergebnis  :  str = "SupportIT-Ausrüstung"
