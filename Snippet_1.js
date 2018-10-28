c1aa3800376bce3504ac077761b8484dcf021156
Nicolas Sauter <sauternic@gmail.com>
2018-10-28 16:48:21 +0100

    Update Snippet_1.js

--- Snippet_1.js
+++ Snippet_1.js
@@ -1,12 +1,18 @@
-//Decodieren von einem kontake.vcf File :)
+//Decodieren von einem Kontake.vcf File :)
 
 //Beispiel  :  NCHARSET=UTF-8ENCODING=QUOTED-PRINTABLE:=53=75=70=70=6F=72=74=49=54=2D=41=75=73=72=C3=BC=73=74=75=6E=67
 
 
 //Vorgehen
 var str = "=53=75=70=70=6F=72=74=49=54=2D=41=75=73=72=C3=BC=73=74=75=6E=67";
+
 //Hier werden einfach '=' durch '%' ersetzt! ;)
 str = str.replace(/={1}/g, '%');
+
+//Zwischen Ergebniss:
+//"%53%75%70%70%6F%72%74%49%54%2D%41%75%73%72%C3%BC%73%74%75%6E%67"
+
+//Jetzt ist es kompatibel für decodeURI()
 str = decodeURI(str);
 
-//Ergebniss  : "SupportIT-Ausrüstung"
+//Ergebniss  :  str = "SupportIT-Ausrüstung"
